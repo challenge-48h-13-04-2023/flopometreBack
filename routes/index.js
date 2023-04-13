e46a6ab76f8b3013757b4077179527466184a2d6
Nino <nino.didier@live.fr>
2023-04-13 14:26:41 +0200

    added check username has already participated route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var StatusCodes = require('http-status-codes');
 var router = express.Router();
+const { Sequelize } = require("sequelize");
+const { sequelize } = require("../models");
+const Resultat = require("../models/resultat")(sequelize, Sequelize.DataTypes,Sequelize.Model);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +12,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/startquiz', function(req, res, next) {
   res.status(StatusCodes.StatusCodes.OK).json("test");
+});
+
+router.get('/username_exists/:username', async function(req, res, next) {
+  const username = req.params.username;
+  const result = await Resultat.findOne({where: {username: username}});
+  var found = false;
+  if (result != null) {
+    found = true;
+  }
+  res.status(StatusCodes.StatusCodes.OK).json(found);
 });
 
 module.exports = router;
